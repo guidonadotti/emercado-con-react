a01c8defaebd51eb128998487994e786fbd42680
Guido Nadotti <gpmnadotti@gmail.com>
2023-07-26 17:38:50 -0300

    Comentarios y demás cosas

--- src/components/ProductInfo/IngresarComentario.jsx
+++ src/components/ProductInfo/IngresarComentario.jsx
@@ -4,6 +4,19 @@ import Estrellas from "./Estrellas";
 import { EstrellasContext } from "../../contexts/EstrellasContext";
 import { useParams } from "react-router-dom";
 import { ProductContext } from "../../contexts/ProductContext";
+import { LoginContext } from "../../contexts/LoginContext";
+import gender from "gender-es";
+
+function formatDateToYYYYMMDDHHMMSS(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
 
 function IngresarComentario() {
   const { id } = useParams();
@@ -12,24 +25,35 @@ function IngresarComentario() {
   const {
     producto: { name = "producto" },
   } = useContext(ProductContext);
+  const { user, getActiveUsername } = useContext(LoginContext);
 
   const handleInputChange = (event) => {
     setText(event.target.value);
     event.target.style.height = "auto";
     event.target.style.height = event.target.scrollHeight + "px";
   };
+
   function handleSubmit(e) {
     e.preventDefault();
-    const datos = { ...Object.fromEntries(new FormData(e.target)) };
-    console.log(datos);
+    if (!user) {
+      console.log("Tenés que estar logueado, pelotudo");
+      return;
+    }
 
     const datosComentario = {
-      producto: id,
+      product: id,
+      score: chequeado,
+      description: text.trim(),
+      dateTime: formatDateToYYYYMMDDHHMMSS(new Date()),
+      user: getActiveUsername({ email: user }),
     };
+
+    console.log(datosComentario);
 
     setText("");
     setChequeado("");
   }
+  const articulo = gender.definiteArticle(name.split(" ")[0]);
 
   return (
     <Form noValidate onSubmit={handleSubmit}>
@@ -38,7 +62,7 @@ function IngresarComentario() {
           <Form.Group className="mb-3">
             <Form.Control
               as="textarea"
-              placeholder={`El ${name} me pareció...`}
+              placeholder={`${articulo} ${name} me pareció...`}
               required
               rows={2}
               value={text}
